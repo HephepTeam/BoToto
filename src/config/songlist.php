feadf69463d140c271ae066a93c74c5a06d1fb5c
Jules <jules.primo@mon-avocat.fr>
2017-11-03 18:00:22 +0100

    little songlist for test

---
+++ src/config/songlist.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Africa' => 'https://youtu.be/FTQbiNvZqaY',
+    '99' => 'https://youtu.be/qhw-XlTMB5I',
+    'St. George and the Dragon' => 'https://youtu.be/Dl_VL5LR-do',
+    'Hydra' => 'https://youtu.be/DS_-j7tv_MQ',
+    'Stranger In Town' => 'https://youtu.be/RSeldKAqM-w',
+    'Rosanna' => 'https://youtu.be/qmOLtTGvsbM',
+];
