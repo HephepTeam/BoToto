bb830523147e42707e9785499dddabbac13c1041
Jules <julesgil.primo@gmail.com>
2017-11-04 15:34:11 +0100

    License

--- public/main.php
+++ public/main.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * (c) Jules-Gil Primo <julesgil.primo@gmail.com>
+ *
+ * Licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require __DIR__ . '/../vendor/autoload.php';
 
 use BoToto\{
